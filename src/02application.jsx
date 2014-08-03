afa22b00307ed8e53715e2d2c7a61057e13d6dce
SlavaBuck <slava.boyko@hotmail.com>
2014-08-03 05:05:27 +0300

    update documentation

--- src/02application.jsx
+++ src/02application.jsx
@@ -1,7 +1,7 @@
 ﻿/**************************************************************************
  *  02application.jsx
  *  DESCRIPTION: BuilderApplication: Основной класс приложения 
- *  @@@BUILDINFO@@@ 02application.jsx 1.80 Sat Aug 02 2014 21:26:26 GMT+0300
+ *  @@@BUILDINFO@@@ 02application.jsx 1.82 Sat Aug 02 2014 21:26:26 GMT+0300
  * 
  * NOTICE: 
  * 
@@ -20,8 +20,8 @@ function BuilderApplication (wtype) { // wtype = dialog || palette
         wtype = (wtype) || "dialog";
     BuilderApplication.prototype.__super__.constructor.call(this, {
     name:"Dialog Builder",
-    version:"1.80",
-    caption:"1.80 Dialog Builder (build 0802, MVC v"+MVC.version+", MVC.DOM v"+MVC.DOM.version+", SimpleUI v"+SUI.version+")",
+    version:"1.82",
+    caption:"1.82 Dialog Builder (build 0802, MVC v"+MVC.version+", MVC.DOM v"+MVC.DOM.version+", SimpleUI v"+SUI.version+")",
     view:wtype + "{spacing:2, margins:[5,5,5,5], orientation:'column', alignChildren:'top', properties:{resizeable: true, closeButton:true, maximizeButton:true }, \
                       pCaption:Panel { margins:[0,1,5,1], spacing:2,alignment:['fill','top'], orientation:'row'}, \
                       pMain:Panel { margins:[0,0,0,0], spacing:0, alignment:['fill','fill'], orientation:'row', \
@@ -1257,7 +1257,6 @@ BuilderApplication.prototype.paste = function() {
     var control = doc.activeContainer[newjsname] = doc.activeContainer.add(rcControl);
     doc.window.layout.layout(true);
     
-   
     if (evalcode) {
         // Трансформируем ссылки старого окна в контекст нового окна
         var pattern = evalcode.match(new RegExp("^\\s?var.+= (.+)\\."+jsname, "m")),
